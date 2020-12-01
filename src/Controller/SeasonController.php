0413a38b37f2c5a2372fb53d67ae87c162461d33
JeremyMordo <jeje.mordo@orange.fr>
2020-12-01 21:56:06 +0100

    quest 12

---
+++ src/Controller/SeasonController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Season;
+use App\Form\SeasonType;
+use App\Repository\SeasonRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/season", name="season_")
+ */
+
+class SeasonController extends AbstractController
+{
+    /**
+     * @Route("/", name="index", methods={"GET"})
+     */
+    public function index(SeasonRepository $seasonRepository): Response
+    {
+        return $this->render('season/index.html.twig', [
+            'seasons' => $seasonRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $season = new Season();
+        $form = $this->createForm(SeasonType::class, $season);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($season);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('season/new.html.twig', [
+            'season' => $season,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="show", methods={"GET"})
+     * @ParamConverter("season", class="App\Entity\Season", options={"mapping": {"id": "id"}})
+     */
+    public function show(Season $season): Response
+    {
+        return $this->render('season/show.html.twig', [
+            'season' => $season,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="edit", methods={"GET","POST"})
+     * @ParamConverter("season", class="App\Entity\Season", options={"mapping": {"id": "id"}})
+     */
+    public function edit(Request $request, Season $season): Response
+    {
+        $form = $this->createForm(SeasonType::class, $season);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('index');
+        }
+
+        return $this->render('season/edit.html.twig', [
+            'season' => $season,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Season $season): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$season->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($season);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('index');
+    }
+}
