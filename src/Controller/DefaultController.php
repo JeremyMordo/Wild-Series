ba72d398ed88fe50c9188cca10c84e05e4e03518
JeremyMordo <jeje.mordo@orange.fr>
2021-01-02 11:51:53 +0100

    quest 26

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Program;
+use App\Repository\CategoryRepository;
 
 class DefaultController extends AbstractController
 {
@@ -23,6 +24,13 @@ class DefaultController extends AbstractController
         return $this->render('index.html.twig', [
             'website' => 'Wild Séries',
             'programs' => $programs,
+        ]);
+    }
+
+    public function navbarTop(CategoryRepository $categoryRepository): Response
+    {
+        return $this->render('layout/navbartop.html.twig', [
+            'categories' => $categoryRepository->findBy([], ['id' => 'DESC'])
         ]);
     }
 }
